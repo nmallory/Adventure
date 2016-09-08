92358d484bd696f386822e88beaa406b8038ef94
Nicole Mallory <nmall76@hotmail.com>
2016-09-08 16:03:23 -0400

    Changed the story from a dragon to a zombie

--- Adventure.java
+++ Adventure.java
@@ -6,12 +6,13 @@ public class Adventure {
 
 	public static void main(String[] args) {
 		
-		String name = "";
-		String yes = "Yes";
-		String answer = "";
-		String weapon = "";
-		String color = "";
-		int option = 0; 
+		String name;
+		String yes;
+		String answer;
+		String weapon;
+		String color;
+		String option; 
+		String dragon; 
 	
 		
 		//Create scanner 
@@ -40,70 +41,77 @@ public class Adventure {
 	
 				case  "run away":
 					answer  = "run away"; //Figure out how to terminate program if user enters run away
-						break;
-						
+					break;
+					
 				}
 			
 				System.out.println("You approach the dragon. You see that he has heads. (enter '1' or '2' or '3'): ");
-				option = input.nextInt();	
+				option = input.nextLine();	
 				
 				switch(option){
-				case 1:
-					System.out.println("No one has ever faced a 1-headed dragon before! Choose your weapon. (enter 'slingshot' or 'sword' or 'bow and arrow')");
+				case "1":
+					System.out.println("No one has ever faced a 1-headed dragon before!");
 					break;
 					
-				case 2:
-					System.out.println("No one has ever faced a 2-headed dragon before! Choose your weapon. (enter 'slingshot' or 'sword' or 'bow and arrow')");
+				case "2":
+					System.out.println("No one has ever faced a 2-headed dragon before!");
 					break;
-					
-				case 3:
-					System.out.println("No one has ever faced a 3-headed dragon before! Choose your weapon. (enter 'slingshot' or 'sword' or 'bow and arrow')");
+		
+				case "3":
+					System.out.println("No one has ever faced a 3-headed dragon before!");
 					break;
 				
 				}
 				
+				System.out.println("Choose your weapon. (enter 'slingshot' or 'sword' or 'bow and arrow'):");
 				weapon = input.nextLine();
-				input.nextLine();		
+				//input.nextLine();		
 				
 				switch(weapon){
 				case "slingshot":
-					
-					System.out.println("No one has ever faced a 1-headed dragon before! Choose your weapon. (enter 'slingshot' or 'sword' or 'bow and arrow'");
+					System.out.println("Armed with your slingshot, you approach the dragon. You can feel its fiery breath as you get closer. It stares at you with its eyes (enter 'red' or 'blue': )");
 					break;
 					
 				case "sword":
-					
-					System.out.println("No one has ever faced a 1-headed dragon before! Choose your weapon. (enter 'slingshot' or 'sword' or 'bow and arrow'");
+					System.out.println("Armed with your sword, you approach the dragon. You can feel its fiery breath as you get closer. It stares at you with its eyes (enter 'red' or 'blue': )");
 					break;
 					
 				case "bow and arrow":
-					System.out.println("No one has ever faced a 1-headed dragon before! Choose your weapon. (enter 'slingshot' or 'sword' or 'bow and arrow'");
+					System.out.println("Armed with your bow and arrow, you approach the dragon. You can feel its fiery breath as you get closer. It stares at you with its eyes (enter 'red' or 'blue': )");
 					break;
 				
-				}
 					
-				System.out.println("Armed with your bow and arrow, you approach the dragon. You can feel its fiery breath as you get closer. It stares at you with its eyes (enter 'red' or 'blue')");
+				}
+				
 				
 				color = input.nextLine();
-				input.nextLine();
+				//input.nextLine();
+				
 				
 				switch(color){
-				
 				case "red":
+					color = "red";
 					System.out.println("Oh thank goodness! Red-eyed dragons are friendly. You pet it and become friends.");
 					break;
 				
 				case "blue":
-					System.out.println("Oh no! Blue-eyed dragons are mean. You pet it and it growls.");
+					color = "blue";
+					System.out.println("Oh no! Blue-eyed dragons are mean. You will not become friends.");
 					break;
 				}
 				
+				System.out.println("You name the dragon (enter a name:) ");
+				dragon = input.nextLine();
+				switch(dragon){
 				
-			}
+				case "dragon":
+					break;
+				
+				}
+				
+				System.out.println("" + "" +name  +dragon   +"live happily ever after.");
+				
 				
 		}
-		
+	}
 }
-		
-	
-	
